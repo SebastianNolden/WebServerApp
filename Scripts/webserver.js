03b71aaf26ecd22d402ed9ff957f447a185f9633
Sebastian Gruno <sebastian.gruno@smail.inf.h-brs.de>
2018-02-20 21:42:22 +0100

    added socket.io and a ToDo to webserver.js

--- Scripts/webserver.js
+++ Scripts/webserver.js
@@ -1,11 +1,30 @@
 var express = require("express");
 var app = express();
 
-//ToDo: Find a way to load a html file
-app.get("/", function(req, res) {
-	res.send("\\..\\index.html");
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+
+//My port for localhost
+const PORT = 3000;
+
+server.listen(process.env.PORT || PORT);
+
+console.log(`Server is running on Port ${PORT}`);
+
+function dirChangeForIndex(){
+	let string = __dirname;
+	let pos = string.lastIndexOf("\\");
+	return string.substring(0,42);
+}
+
+app.get("/", function(req, res){
+	str = dirChangeForIndex();
+	res.sendFile(str + "index.html")
 });
 
-app.listen(3000, function() {
-	console.log("Webserver listening on port 3000!");
+//ToDo: Chat auf Socket Basis - socket.io! Gerne auch mit jQuery und Bootstrap
+//https://www.youtube.com/watch?v=tHbCkikFfDE
+// scheint die Schnittstelle zum Senden von Nachrichten und so zu sein!
+io.sockets.on("connection", function(socket){
+
 });
