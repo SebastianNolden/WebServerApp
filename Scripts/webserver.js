589e2394bb80196f52f4402d18127036c1a5750b
Sebastian Gruno <sebastian.gruno@smail.inf.h-brs.de>
2018-02-20 22:41:21 +0100

    first Webfunctions as Chat Front End + Server can recieve a message from Front End

--- Scripts/webserver.js
+++ Scripts/webserver.js
@@ -11,20 +11,32 @@ server.listen(process.env.PORT || PORT);
 
 console.log(`Server is running on Port ${PORT}`);
 
-function dirChangeForIndex(){
-	let string = __dirname;
-	let pos = string.lastIndexOf("\\");
-	return string.substring(0,42);
+function dirChangeForIndex() {
+  let string = __dirname;
+  let pos = string.lastIndexOf("\\");
+  return string.substring(0, 42);
 }
 
-app.get("/", function(req, res){
-	str = dirChangeForIndex();
-	res.sendFile(str + "index.html")
+app.get("/", function(req, res) {
+  str = dirChangeForIndex();
+  res.sendFile(str + "index.html");
 });
 
 //ToDo: Chat auf Socket Basis - socket.io! Gerne auch mit jQuery und Bootstrap
 //https://www.youtube.com/watch?v=tHbCkikFfDE
 // scheint die Schnittstelle zum Senden von Nachrichten und so zu sein!
-io.sockets.on("connection", function(socket){
+io.sockets.on("connection", function(socket) {
+
+  console.log("A User connected to the Chat.");
+
+  //Disconnect
+  socket.on("disconnect", function() {
+    console.log("A User disconnected.");
+  });
+
+
+  socket.on("chat message", function(msg) {
+    console.log('message: ' + msg);
+  });
 
 });
